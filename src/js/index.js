411445eac414998451eb7daa975e6c26bf777539
Alan Da Silva <as38#aluno.ifal.edu.br>
2024-10-13 14:08:17 -0300

    Refatorando JavaScript

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,34 @@ const listaSelecaoPokedevs = document.querySelectorAll(".pokedev");
 listaSelecaoPokedevs.forEach(pokemon => {
     pokemon.addEventListener("click", () => {
         //Cartão
-        const cartaoPokemonAtiva = document.querySelector(".ativa")
-        cartaoPokemonAtiva.classList.remove("ativa")
-        const idPokemonSelecionado = pokemon.attributes.id.value;
-        const idDoCartaoPokemonParaAbrir = "cartao-" + idPokemonSelecionado;
-        const cartaoPokemonAbrir = document.getElementById(idDoCartaoPokemonParaAbrir);
-        cartaoPokemonAbrir.classList.add("ativa");
+        esconderCartaoPokemon();
+        const idPokemonSelecionado = mostrarCartaoPokemon(pokemon);
+
         //Botão
-        const pokemonAtivoListagem = document.querySelector(".ativo");
-        pokemonAtivoListagem.classList.remove("ativo");
-        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado);
-        pokemonSelecionadoNaListagem.classList.add("ativo")
+        desativarPokemonNaListagem();
+        ativaPokemonNaListagem(idPokemonSelecionado);
     })
 });
+
+function ativaPokemonNaListagem(idPokemonSelecionado) {
+    const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado);
+    pokemonSelecionadoNaListagem.classList.add("ativo");
+}
+
+function desativarPokemonNaListagem() {
+    const pokemonAtivoListagem = document.querySelector(".ativo");
+    pokemonAtivoListagem.classList.remove("ativo");
+}
+
+function mostrarCartaoPokemon(pokemon) {
+    const idPokemonSelecionado = pokemon.attributes.id.value;
+    const idDoCartaoPokemonParaAbrir = "cartao-" + idPokemonSelecionado;
+    const cartaoPokemonAbrir = document.getElementById(idDoCartaoPokemonParaAbrir);
+    cartaoPokemonAbrir.classList.add("ativa");
+    return idPokemonSelecionado;
+}
+
+function esconderCartaoPokemon() {
+    const cartaoPokemonAtiva = document.querySelector(".ativa");
+    cartaoPokemonAtiva.classList.remove("ativa");
+}
